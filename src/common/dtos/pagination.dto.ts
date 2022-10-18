b0642fe9b6b069151e353ed1de871ddb2c159a80
Joel Liranzo <joeliranzo@gmail.com>
2022-10-17 22:26:34 -0400

    initial commit

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,12 @@
+import { IsOptional, IsPositive, Min } from "class-validator"
+
+export class PaginationDto {
+	@IsOptional()
+	@IsPositive()
+	// Transformar	
+	limit?: number
+
+	@IsOptional()
+	@Min(0)
+	offset?: number
+}
---
+++ src/products/products.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product, ProductImage } from './entities';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+
+@Module({
+	controllers: [ProductsController],
+	providers: [ProductsService],
+	exports: [TypeOrmModule,ProductsService],
+	imports: [
+		TypeOrmModule.forFeature([Product, ProductImage])
+	]
+})
+export class ProductsModule {}
